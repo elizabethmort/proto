514e78839f25f48a9eb6885c325f5688a7f09ddf
Tony Kay <tony.kay@gmail.com>
2014-09-30 19:23:00 -0700

    added some task handling

--- js/app.js
+++ js/app.js
@@ -1,27 +1,29 @@
 angular.module('app', []).controller('main', function ($scope) {
+    $scope.taskRow = 0;
     $scope.row = -1;
     $scope.col = -1;
 
     $scope.highlightCard = function () {
-        if ($scope.col != -1 && $scope.row != -1) {
-            angular.forEach($scope.lanes, function (l, key, obj) {
-                angular.forEach(l.cards, function (c, key, obj) {
-                    console.log(c);
-                    c['highlighted'] = false;
-                })
-            });
-            var nrows = $scope.lanes[$scope.col]['cards'].length;
-            var clampedRow = $scope.row;
-            if($scope.row >= nrows)
-              clampedRow = nrows-1;
-
-            $scope.lanes[$scope.col]['cards'][clampedRow].highlighted = true;
-            console.log("Highlighting ", $scope.row, $scope.col);
-        }
-    }
+        angular.forEach($scope.lanes, function (l, key, obj) {
+            angular.forEach(l.cards, function (c, key, obj) {
+                c['highlighted'] = false;
+            })
+        });
+        var card = $scope.currentCard();
+        if (card)
+            card.highlighted = true;
+    };
     $scope.lanes = [
         { cards: [
-            { id: 1, title: "Card 1" },
+            { id: 1, title: "Card 1",
+                tasks: {
+                    visible: false,
+                    items: [
+                        {id: 1, description: 'pick nose' },
+                        {id: 2, description: 'flick boogers' }
+                    ]
+                }
+            },
             { id: 2, title: "Card 2" }
         ] },
         { cards: [
@@ -31,15 +33,90 @@ angular.module('app', []).controller('main', function ($scope) {
             { id: 6, title: "Card 6" }
         ]}
     ];
-    $scope.$on("down", function () {
+    $scope.currentCard = function () {
+        if ($scope.row == -1 || $scope.col == -1) return undefined;
+
+        var nrows = $scope.lanes[$scope.col]['cards'].length;
+        var clampedRow = $scope.row;
+        if ($scope.row >= nrows)
+            clampedRow = nrows - 1;
+
+        return $scope.lanes[$scope.col]['cards'][clampedRow];
+    };
+
+    $scope.$on("escape", function () {
+        var card = $scope.currentCard();
+        if (card && card.tasks && card.tasks.visible)
+            $scope.toggleTasks();
+        else {
+            $scope.$apply(function () {
+                $scope.row = -1;
+                $scope.col = -1;
+                $scope.highlightCard();
+            });
+        }
+    });
+
+    $scope.toggleTasks = function () {
+        var card = $scope.currentCard();
+        if (!card) return;
+
+        $scope.$apply(function () {
+            $scope.taskRow = 0;
+            if (!card.tasks)
+                card.tasks = {};
+            card.tasks.visible = !card.tasks.visible;
+        });
+    };
+    $scope.$on("toggle-tasks", function () {
+        $scope.toggleTasks();
+    });
+    $scope.downCard = function () {
         if ($scope.col == -1) $scope.col = 0;
         var nrows = $scope.lanes[$scope.col]['cards'].length;
         console.log(nrows);
-        if ($scope.row < nrows-1)
+        if ($scope.row < nrows - 1)
             $scope.row++;
         $scope.$apply(function () {
             $scope.highlightCard();
         });
+    };
+
+    $scope.upTask = function () {
+        var card = $scope.currentCard();
+        if (!card) return;
+        var todo = card.tasks.items;
+        if ($scope.taskRow > 0)
+            $scope.$apply(function () {
+                $scope.taskRow--;
+            });
+    };
+
+    $scope.downTask = function () {
+        var card = $scope.currentCard();
+        if (!card) return;
+        var todo = card.tasks.items;
+        var nitems = todo.length;
+        if ($scope.taskRow < nitems - 1)
+            $scope.$apply(function () {
+                $scope.taskRow++;
+            });
+    };
+
+    $scope.$on("down", function () {
+        var card = $scope.currentCard();
+        if (card && card.tasks && card.tasks.visible)
+            $scope.downTask();
+        else
+            $scope.downCard();
+    });
+    $scope.$on("mark", function () {
+        var card = $scope.currentCard();
+        if (card && card.tasks && card.tasks.visible) {
+            $scope.$apply(function () {
+                card.tasks.items[$scope.taskRow].complete = !card.tasks.items[$scope.taskRow].complete;
+            });
+        }
     });
     $scope.$on("left", function () {
         if ($scope.row == -1) $scope.row = 0;
@@ -57,16 +134,24 @@ angular.module('app', []).controller('main', function ($scope) {
             $scope.highlightCard();
         });
     });
-    $scope.$on("up", function () {
+    $scope.upCard = function () {
         if ($scope.col == -1) $scope.col = 0;
         if ($scope.row > 0)
             $scope.row--;
         var nrows = $scope.lanes[$scope.col]['cards'].length;
-        if($scope.row >= nrows-1)
-          $scope.row = nrows-2;
+        if ($scope.row >= nrows - 1)
+            $scope.row = nrows - 2;
         $scope.$apply(function () {
             $scope.highlightCard();
         });
+    }
+
+    $scope.$on("up", function () {
+        var card = $scope.currentCard();
+        if (card && card.tasks && card.tasks.visible)
+            $scope.upTask();
+        else
+            $scope.upCard();
     });
 }).directive('tkFocus', function () {
     return {
@@ -81,6 +166,15 @@ angular.module('app', []).controller('main', function ($scope) {
                     scope.$emit("left");
                 else if (String.fromCharCode(event.charCode) == 'l')
                     scope.$emit("right");
+                else if (String.fromCharCode(event.charCode) == 't')
+                    scope.$emit("toggle-tasks");
+                else if (String.fromCharCode(event.charCode) == 'x')
+                    scope.$emit("mark");
+                else console.log(event.charCode);
+            });
+            jQuery(element).keyup(function (event) {
+                if (event.keyCode == 27)
+                    scope.$emit("escape");
             });
 
         }
